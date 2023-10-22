b4ceb94deb99c30023487aaeda70ac7b8d9b199f
Glacy Gomes <glacy.kelly3@gmail.com>
2023-10-22 00:15:28 -0300

    feat: adicionando HttpClientModule

--- apps/pokedex/src/app/app.module.ts
+++ apps/pokedex/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HomeModule } from './home/home.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
@@ -12,6 +13,7 @@ import { HomeModule } from './home/home.module';
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
     HomeModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
