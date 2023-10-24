81e379279b755306951ea35530ca174c0ea3ee19
Glacy Gomes <glacy.kelly3@gmail.com>
2023-10-23 23:19:10 -0300

    feature: montando layout da pagina de details

--- libs/pokemon-info/src/lib/pokemon-info/pokemon-info.component.ts
+++ libs/pokemon-info/src/lib/pokemon-info/pokemon-info.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'pokedex-pokemon-info',
   templateUrl: './pokemon-info.component.html',
   styleUrls: ['./pokemon-info.component.scss'],
 })
-export class PokemonInfoComponent {}
+export class PokemonInfoComponent {
+  constructor(private router: Router) {}
+
+  public navigateToHome(): void {
+    this.router.navigate(['/']);
+  }
+}
