1b0a952fa052a8607a209e3aa8b227617d21c0ab
Glacy Gomes <glacy.kelly3@gmail.com>
2023-10-29 10:28:46 -0300

    feature: nova pipe converte hex para rgb color

---
+++ libs/shared/pipes/src/lib/hex-to-rgb/hex-to-rgb.pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+interface RgbColors {
+  [key: string]: string;
+}
+const rgbColor: RgbColors = {
+  grass: '116, 203, 72',
+  poison: '164, 62, 158',
+  fire: '245, 125, 49',
+  water: '100, 147, 235',
+  bug: '167, 183, 35',
+  eletric: '249, 207, 48',
+  ghost: '112, 85, 155',
+  normal: '170, 166, 127',
+  psyhic: '251, 85, 132',
+  steel: '183, 185, 208',
+};
+
+@Pipe({
+  name: 'hexcolorToRgb',
+})
+export class HexToRgbPipe implements PipeTransform {
+  transform(value: string): string {
+    return `rgb(${rgbColor[value]}, 0.2)`;
+  }
+}
--- libs/shared/pipes/src/lib/pipes.module.ts
+++ libs/shared/pipes/src/lib/pipes.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BackgroundColorPipe } from './background-color/background-color.pipe';
+import { HexToRgbPipe } from './hex-to-rgb/hex-to-rgb.pipe';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [BackgroundColorPipe],
-  exports: [BackgroundColorPipe],
+  declarations: [BackgroundColorPipe, HexToRgbPipe],
+  exports: [BackgroundColorPipe, HexToRgbPipe],
 })
 export class PipesModule {}
