73180568f9cd2add9d87e8bbc6c7eb256ec8e758
Glacy Gomes <glacy.kelly3@gmail.com>
2023-11-15 12:29:22 -0300

    feature: exporting new pipes

--- libs/shared/pipes/src/lib/pipes.module.ts
+++ libs/shared/pipes/src/lib/pipes.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BackgroundColorPipe } from './background-color/background-color.pipe';
 import { HexToRgbPipe } from './hex-to-rgb/hex-to-rgb.pipe';
+import { ConvertStatsToUpperCasePipe } from './convert-stats-to-upper-case/convert-stats-to-upper-case.pipe';
+import { PadStartPipe } from './pad-start/pad-start.pipe';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [BackgroundColorPipe, HexToRgbPipe],
-  exports: [BackgroundColorPipe, HexToRgbPipe],
+  declarations: [
+    BackgroundColorPipe,
+    HexToRgbPipe,
+    ConvertStatsToUpperCasePipe,
+    PadStartPipe,
+  ],
+  exports: [
+    BackgroundColorPipe,
+    HexToRgbPipe,
+    ConvertStatsToUpperCasePipe,
+    PadStartPipe,
+  ],
 })
 export class PipesModule {}
