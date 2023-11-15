f13502c477e209a027e11b0f876f3bfe20e41c7b
Glacy Gomes <glacy.kelly3@gmail.com>
2023-11-15 12:36:46 -0300

    feature: implementing variable of layout

--- libs/pokemon-info/src/lib/pokemon-info/pokemon-info.component.ts
+++ libs/pokemon-info/src/lib/pokemon-info/pokemon-info.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { PokemonsService } from '@pokedex/services';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'pokedex-pokemon-info',
   templateUrl: './pokemon-info.component.html',
   styleUrls: ['./pokemon-info.component.scss'],
 })
-export class PokemonInfoComponent {
-  constructor(private router: Router) {}
+export class PokemonInfoComponent implements OnDestroy {
+  public pokemonInfo$!: Observable<any>;
+  private pokemonId!: string;
+  private routeSub!: Subscription;
+
+  constructor(
+    private router: Router,
+    private pokemonService: PokemonsService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.activatedRoute.params.subscribe((params) => {
+      this.pokemonId = params['id'];
+    });
+    this.pokemonInfo$ = this.pokemonService.getPokemonById(this.pokemonId);
+    this.pokemonInfo$.subscribe((x) => console.log(x));
+  }
+
+  ngOnDestroy() {
+    if (this.routeSub) {
+      this.routeSub.unsubscribe();
+    }
+  }
 
   public navigateToHome(): void {
     this.router.navigate(['/']);
