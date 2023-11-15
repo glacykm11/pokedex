5e36c15aeedcce6da8fec999f18a3b2562494164
Glacy Gomes <glacy.kelly3@gmail.com>
2023-11-15 12:28:37 -0300

    feature: implementing padStart pipe

---
+++ libs/shared/pipes/src/lib/pad-start/pad-start.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'padStart',
+})
+export class PadStartPipe implements PipeTransform {
+  transform(value: number | string): string {
+    const valueWithPadStart = value.toString().padStart(3, '0');
+
+    return valueWithPadStart;
+  }
+}
