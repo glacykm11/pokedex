e46f6c4612c9460707c4b4eeaa1ff906578c7167
Glacy Gomes <glacy.kelly3@gmail.com>
2023-11-15 12:05:18 -0300

    fix: adicionando e ajustando cores

--- libs/shared/pipes/src/lib/hex-to-rgb/hex-to-rgb.pipe.ts
+++ libs/shared/pipes/src/lib/hex-to-rgb/hex-to-rgb.pipe.ts
@@ -9,11 +9,16 @@ const rgbColor: RgbColors = {
   fire: '245, 125, 49',
   water: '100, 147, 235',
   bug: '167, 183, 35',
-  eletric: '249, 207, 48',
+  electric: '249, 207, 48',
   ghost: '112, 85, 155',
   normal: '170, 166, 127',
   psyhic: '251, 85, 132',
   steel: '183, 185, 208',
+  rock: '182, 158, 49',
+  ground: '224, 192, 104',
+  fairy: '224, 58, 131',
+  fighting: '255, 80, 31',
+  psychic: '248, 88, 136',
 };
 
 @Pipe({
